e25dc6ccde5d4875a87d76ffa1208134801e18e6
Jamie <40514090@live.napier.ac.uk>
2024-01-11 12:23:20 +0000

    Added Screens for managing Jobs + Offices
    Implemented Create Functionality for Offices
    
    Implemented Create Functionality for Jobs

---
+++ jobListScripts.js
@@ -0,0 +1,136 @@
+let selectedRow = '';
+let allJobData;
+
+function getAllJobs(){
+    fetch('https://localhost:5003/api/jobs')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Handle the data received from the API
+    console.log('Data received:', data);
+    // You can perform operations with the 'data' here
+    allJobData = data;
+    populateTable(data);
+  })
+  .catch(error => {
+    // Handle errors that may occur during the fetch operation
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
+function populateTable(data){
+    const tableBody = document.getElementById('jobTB');
+    //tableBody.innerHTML = '';
+
+    //For Each loop
+    data.forEach(job => {
+        //Adds a new row to the table for each employee
+        const newRow = document.createElement('tr');
+
+        //Self Reminder - Remember to use backticks ` not single or double quotes ' "
+        newRow.innerHTML = `<td data-th='Job ID'>${job.id}</td> <td data-th='Job Title'>${job.jobTitle}</td> <td data-th='Department'>${job.department}</td> <td data-th='Salary'>${job.salary}</td>`;
+        
+        // Add a click event listener to each row
+        newRow.addEventListener('click', () => {
+            // Remove 'clicked' class from all rows
+            const allRows = document.querySelectorAll('tr');
+            allRows.forEach(row => {
+                row.classList.remove('clicked');
+            }); 
+            newRow.classList.add('clicked'); 
+
+
+            selectedRow = job.id;
+            console.log('Row clicked! Job ID:', selectedRow);
+            // Perform actions when a row is clicked
+        });
+        
+        tableBody.appendChild(newRow);
+    });
+}
+function addNewJobtoTable(newJobData)
+{
+    const tableBody = document.getElementById('jobTB');
+
+    const newRow = document.createElement('tr');
+
+    //Self Reminder - Remember to use backticks ` not single or double quotes ' "
+    newRow.innerHTML = `<td data-th='Job ID'>${newJobData.id}</td> <td data-th='Job Title'>${newJobData.jobTitle}</td> <td data-th='Department'>${newJobData.department}</td> <td data-th='Salary'>${newJobData.salary}</td>`;
+    
+    // Add a click event listener to each row
+    newRow.addEventListener('click', () => {
+        // Remove 'clicked' class from all rows
+        const allRows = document.querySelectorAll('tr');
+        allRows.forEach(row => {
+            row.classList.remove('clicked');
+        }); 
+        newRow.classList.add('clicked'); 
+
+
+        selectedRow = newJobData.id;
+        console.log('Row clicked! Job ID:', selectedRow);
+        // Perform actions when a row is clicked
+    });
+    
+    tableBody.appendChild(newRow);
+
+}
+function createJobDto() {
+    const jobDto = {
+        jobTitle: document.getElementById('jobTitle').value,
+        Department: document.getElementById('jobDepartment').value,
+        salary: parseInt(document.getElementById('jobSalary').value)
+    };
+    return jobDto;
+}
+
+function createNewJob(){
+    const jobData = createJobDto(); //Getting the Job data to send to the api
+
+    fetch('https://localhost:5003/api/jobs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jobData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        createdJobID = data.id;
+        addNewJobtoTable(data);
+        alert("New Job Created Successfully");
+        closeAddJobModal();
+        // Handle success - do something with the response from the server
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        // Handle error - alert the user or perform other actions
+    });
+
+}
+
+function displayAddJobModal() {
+    var modal = document.getElementById("idAddJob");
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeAddJobModal() {
+    var modal = document.getElementById("idAddJob");
+    modal.style.display = "none";
+
+    // Clear input fields
+    document.getElementById('jobTitle').value = '';
+    document.getElementById('jobDepartment').value = '';
+    document.getElementById('jobSalary').value = '';
+}
---
+++ officeListScripts.js
@@ -0,0 +1,138 @@
+let selectedRow = '';
+let allofficeData;
+
+function getAlloffices(){
+    fetch('https://localhost:5003/api/offices')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Handle the data received from the API
+    console.log('Data received:', data);
+    // You can perform operations with the 'data' here
+    allofficeData = data;
+    populateTable(data);
+  })
+  .catch(error => {
+    // Handle errors that may occur during the fetch operation
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
+function populateTable(data){
+    const tableBody = document.getElementById('officeTB');
+    //tableBody.innerHTML = '';
+
+    //For Each loop
+    data.forEach(office => {
+        //Adds a new row to the table for each employee
+        const newRow = document.createElement('tr');
+
+        //Self Reminder - Remember to use backticks ` not single or double quotes ' "
+        newRow.innerHTML = `<td data-th='office ID'>${office.id}</td> <td data-th='Building Number'>${office.buildingNum}</td> <td data-th='Street'>${office.street}</td> <td data-th='Postcode'>${office.postcode}</td> <td data-th='City'>${office.city}</td> <td data-th='Country'>${office.country}</td>`;
+        
+        // Add a click event listener to each row
+        newRow.addEventListener('click', () => {
+            // Remove 'clicked' class from all rows
+            const allRows = document.querySelectorAll('tr');
+            allRows.forEach(row => {
+                row.classList.remove('clicked');
+            }); 
+            newRow.classList.add('clicked'); 
+
+
+            selectedRow = office.id;
+            console.log('Row clicked! office ID:', selectedRow);
+            // Perform actions when a row is clicked
+        });
+        
+        tableBody.appendChild(newRow);
+    });
+}
+function addNewofficetoTable(newofficeData)
+{
+    const tableBody = document.getElementById('officeTB');
+
+    const newRow = document.createElement('tr');
+
+    //Self Reminder - Remember to use backticks ` not single or double quotes ' "
+    newRow.innerHTML = `<td data-th='office ID'>${newofficeData.id}</td> <td data-th='Building Number'>${newofficeData.buildingNum}</td> <td data-th='Street'>${newofficeData.street}</td> <td data-th='Postcode'>${newofficeData.postcode}</td> <td data-th='City'>${newofficeData.city}</td> <td data-th='Country'>${newofficeData.country}</td>`;
+    
+    // Add a click event listener to each row
+    newRow.addEventListener('click', () => {
+        // Remove 'clicked' class from all rows
+        const allRows = document.querySelectorAll('tr');
+        allRows.forEach(row => {
+            row.classList.remove('clicked');
+        }); 
+        newRow.classList.add('clicked'); 
+
+
+        selectedRow = newofficeData.id;
+        console.log('Row clicked! office ID:', selectedRow);
+        // Perform actions when a row is clicked
+    });
+    
+    tableBody.appendChild(newRow);
+
+}
+function createofficeDto() {
+    const officeDto = {
+        buildingNum: parseInt(document.getElementById('officeNum').value),
+        street: document.getElementById('officeStreet').value,
+        postcode: document.getElementById('officePostcode').value,
+        city: document.getElementById('officeCity').value,
+        country: document.getElementById('officeCountry').value
+    };
+    return officeDto;
+}
+
+function createNewOffice(){
+    const officeData = createofficeDto(); //Getting the office data to send to the api
+
+    fetch('https://localhost:5003/api/offices', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(officeData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        createdofficeID = data.id;
+        addNewofficetoTable(data);
+        alert("New office Created Successfully");
+        closeAddofficeModal();
+        // Handle success - do something with the response from the server
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        // Handle error - alert the user or perform other actions
+    });
+
+}
+
+function displayAddofficeModal() {
+    var modal = document.getElementById("idAddoffice");
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeAddofficeModal() {
+    var modal = document.getElementById("idAddoffice");
+    modal.style.display = "none";
+
+    // Clear input fields
+    document.getElementById('officeTitle').value = '';
+    document.getElementById('officeDepartment').value = '';
+    document.getElementById('officeSalary').value = '';
+}
