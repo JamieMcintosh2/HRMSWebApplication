375d0a0869c21ad74d21d496a7a14441c8bf6d77
Jamie <40514090@live.napier.ac.uk>
2024-01-14 12:48:22 +0000

    Added PATCH functionality to Profile, Address and Emergency Contacts.

--- profileScripts.js
+++ profileScripts.js
@@ -4,6 +4,14 @@ let employeeSelected = JSON.parse(localStorage.getItem('selectedEmployee'));
 let selectedDiv = "profileInfo"; // Default selected div
 
 
+function fillEditFields()
+{
+        //Fill Profile Fields with data from selected employee
+        document.getElementById("fName").value = employeeSelected.fName;
+        document.getElementById("lName").value = employeeSelected.lName;
+        document.getElementById("pNumber").value = employeeSelected.pNumber;
+
+}
 
 function hideAllDivs() {
     const divs = ["profileInfo", "addressInfo", "employmentInfo", "emergencyInfo"];
@@ -26,17 +34,21 @@ function hideAllDivs() {
 function displayEditProfile() {
     hideAllDivs();
     document.getElementById("profileInfo").style.display = "block";
+    selectedDiv = "profileInfo";
     const btnProfile = document.getElementById("btnProfile");
     btnProfile.classList.remove("w3-black");
 
     // Add the new class
     btnProfile.classList.add("w3-white");
     selectedDiv = "profileInfo";
+
+
 }
 
 function displayEditAddress() {
     hideAllDivs();
     document.getElementById("addressInfo").style.display = "block";
+    selectedDiv = "addressInfo";
     const btn = document.getElementById("btnAddress");
     btn.classList.remove("w3-black");
 
@@ -48,6 +60,7 @@ function displayEditAddress() {
 function displayEditEmployment() {
     hideAllDivs();
     document.getElementById("employmentInfo").style.display = "block";
+    selectedDiv = "employmentInfo";
     const btn = document.getElementById("btnEmployment");
     btn.classList.remove("w3-black");
 
@@ -59,12 +72,209 @@ function displayEditEmployment() {
 function displayEditEmerg() {
     hideAllDivs();
     document.getElementById("emergencyInfo").style.display = "block";
+    selectedDiv = "emergencyInfo";
     const btn = document.getElementById("btnEmerg");
     btn.classList.remove("w3-black");
 
     // Add the new class
     btn.classList.add("w3-white");
     selectedDiv = "emergencyInfo";
+}
+
+function checkEmptyFields()
+{
+        //storing all the input/select fields inside the jobAdDiv
+    // and checking if they are empty
+    const fields = document.querySelectorAll('#'+ selectedDiv +' input');
+    let isEmpty = false;
+
+    fields.forEach(field => {
+        if (field.value.trim() === '' && field.hasAttribute('required')) {
+            isEmpty = true;
+            field.classList.add('w3-border-red'); // Add red border to empty required fields
+        } else {
+            field.classList.remove('w3-border-red'); // Remove red border if field is filled or not required
+        }
+    });
+
+    if (isEmpty) {
+        alert('Please fill in all required fields.');
+    }
+    else{
+        return true;
+    }
+}
+
+function UpdateEmergencyInfo()
+{
+    if(checkEmptyFields())
+    {
+        let endpoint = apiAddress + 'emContacts/' + employeeSelected.id;
+        let newFName = document.getElementById('emergFName').value;
+        let newLName = document.getElementById('emergLName').value;
+        //let newRelationship = document.getElementById('relationship').value;
+        let newEmergPhone = document.getElementById('emergPNumber').value;
+
+        const selection = document.getElementById('relationship');
+            //data to send to the api
+        let dataToUpdate = [
+            { op: 'replace', path: '/fName', value: newFName },
+            { op: 'replace', path: '/lName', value: newLName },
+            { op: 'replace', path: '/relationship', value: selection.options[selection.selectedIndex].text },
+            { op: 'replace', path: '/pNumber', value: newEmergPhone }
+        ];
+        patchEmergencyData(dataToUpdate, endpoint);
+    }
+}
+
+function patchEmergencyData(dataToUpdate, endpoint)
+{
+    console.log(dataToUpdate);
+    fetch(endpoint, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers if needed
+        },
+        body: JSON.stringify(dataToUpdate) // Convert your data to JSON
+    })
+    .then(response => {
+    // Handle the response from the server
+        if (response.ok) 
+        {
+            console.log('PATCH request successful');
+            alert('Data Successfully updated')
+            // UPDATE UI
+            let newFName = dataToUpdate.find(item => item.path === '/fName').value;
+            let newLName = dataToUpdate.find(item => item.path === '/lName').value;
+            let newRelationship = dataToUpdate.find(item => item.path === '/relationship').value;
+            let newPNumber = dataToUpdate.find(item => item.path === '/pNumber').value;
+
+            document.getElementById('emergencyFName').textContent = newFName;
+            document.getElementById('emergencyLName').textContent = newLName;
+            document.getElementById('emergRelationship').textContent = newRelationship;
+            document.getElementById('EmergPhoneNum').textContent = newPNumber;
+
+
+        } else {
+            throw new Error('PATCH request failed');
+        }
+    })
+    .catch(error => 
+    {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+}
+
+
+function UpdateAddressInfo()
+{
+    if(checkEmptyFields())
+    {
+        let endpoint = apiAddress + 'addresses/' + employeeSelected.id;
+        let newHouseNum = parseInt(document.getElementById('houseNum').value);
+        let newStreet = document.getElementById('Street').value;
+        let newPostcode = document.getElementById('Postcode').value;
+        let newCity = document.getElementById('City').value;
+        let newCountry = document.getElementById('Country').value;
+            //data to send to the api
+        let dataToUpdate = [
+            { op: 'replace', path: '/houseNum', value: newHouseNum },
+            { op: 'replace', path: '/street', value: newStreet },
+            { op: 'replace', path: '/postcode', value: newPostcode },
+            { op: 'replace', path: '/city', value: newCity },
+            { op: 'replace', path: '/country', value: newCountry }
+        ];
+        patchAddressData(dataToUpdate, endpoint);
+    }
+}
+function patchAddressData(dataToUpdate, endpoint)
+{
+    console.log(dataToUpdate);
+    fetch(endpoint, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers if needed
+        },
+        body: JSON.stringify(dataToUpdate) // Convert your data to JSON
+    })
+    .then(response => {
+    // Handle the response from the server
+        if (response.ok) 
+        {
+            console.log('PATCH request successful');
+            alert('Data Successfully updated')
+            // UPDATE UI
+            let newCity = dataToUpdate.find(item => item.path === '/city').value;
+            let newCountry = dataToUpdate.find(item => item.path === '/country').value;
+
+            document.getElementById('cityCountry').textContent = newCity + ', ' + newCountry;
+
+
+        } else {
+            throw new Error('PATCH request failed');
+        }
+    })
+    .catch(error => 
+    {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+}
+
+function UpdateProfileInfo()
+{
+    if(checkEmptyFields())
+    {
+        let endpoint = apiAddress + 'employees/' + employeeSelected.id;
+        let newFName = document.getElementById('fName').value;
+        let newLName = document.getElementById('lName').value;
+        let newPhoneNumber = document.getElementById('pNumber').value;
+            //data to send to the api
+        let dataToUpdate = [
+            { op: 'replace', path: '/fName', value: newFName },
+            { op: 'replace', path: '/lName', value: newLName },
+            { op: 'replace', path: '/pNumber', value: newPhoneNumber }
+        ];
+        patchProfileData(dataToUpdate, endpoint);
+    }
+}
+
+function patchProfileData(dataToUpdate, endpoint)
+{
+    console.log(dataToUpdate);
+    fetch(endpoint, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers if needed
+        },
+        body: JSON.stringify(dataToUpdate) // Convert your data to JSON
+    })
+    .then(response => {
+    // Handle the response from the server
+        if (response.ok) 
+        {
+            console.log('PATCH request successful');
+            alert('Data Successfully updated')
+            // UPDATE UI
+            let newFName = dataToUpdate.find(item => item.path === '/fName').value;
+            let newLName = dataToUpdate.find(item => item.path === '/lName').value;
+            let newPhoneNumber = dataToUpdate.find(item => item.path === '/pNumber').value;
+            document.getElementById('empName').textContent = newFName + ' ' + newLName;
+            document.getElementById('EmpPhoneNum').textContent = newPhoneNumber;
+
+        } else {
+            throw new Error('PATCH request failed');
+        }
+    })
+    .catch(error => 
+    {
+        console.error('Error:', error);
+        // Handle errors here
+    });
 }
 
 
@@ -134,8 +344,6 @@ function enableEditing() {
         { op: 'replace', path: '/lName', value: newLName },
         { op: 'replace', path: '/Relationship', value: newRelationship },
         { op: 'replace', path: '/pNumber', value: newEmergPhone }
-
-
         ];
             //fName: newFName, lName: newLName, Relationship: newRelationship, pNumber: newEmergPhone
 
@@ -200,6 +408,14 @@ Promise.all(fetchPromises)
         const empCityCountryElement = document.getElementById('cityCountry');
         let cityCountry = addressData.city.concat(", ", addressData.country);
         empCityCountryElement.textContent = cityCountry;
+        //Filling address Edit fields
+        document.getElementById("houseNum").value = addressData.houseNum;
+        document.getElementById("Street").value = addressData.street;
+        document.getElementById("Postcode").value = addressData.postcode;
+        document.getElementById("City").value = addressData.city;
+        document.getElementById("Country").value = addressData.country;
+
+
 
         // Work with emergency contact data
         const relationshipElement = document.getElementById('emergRelationship');
@@ -213,6 +429,28 @@ Promise.all(fetchPromises)
 
         const phoneNumElement = document.getElementById('EmergPhoneNum');
         phoneNumElement.textContent = emergencyData.pNumber;
+        //Filling Edit data Fields
+        document.getElementById("emergFName").value = emergencyData.fName;
+        document.getElementById("emergLName").value = emergencyData.lName;
+        document.getElementById("emergPNumber").value = emergencyData.pNumber;
+        //Select fields were awkward
+        //Had to swap around the values and text data to get them to match
+        //e.g. DB Stores "Wife" not the numeric value which is 4. So I had to remap to "Wife" to the value of 4 - messy oversight
+        // Mapping object for text values to numeric values
+        const relationshipMapping = {
+            "Father": 1,
+            "Mother": 2,
+            "Partner": 3,
+            "Wife": 4,
+            "Husband": 5,
+            "Other": 6
+        };
+        const relationshipFromDB = emergencyData.relationship;
+        document.getElementById("relationship").value = relationshipMapping[relationshipFromDB];
+
+
+
+
 
         // Work with employee job data
         const departmentElement = document.getElementById('jobDepartment');
