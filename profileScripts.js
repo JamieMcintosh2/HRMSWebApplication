a7c5e4aa06fd60988ba8b0e2dc5a226e15acb57d
Jamie <40514090@live.napier.ac.uk>
2024-01-15 11:52:38 +0000

    Added PATCH Job and Office Functionality - Now employee can be changed Job or Office Location

--- profileScripts.js
+++ profileScripts.js
@@ -104,6 +104,88 @@ function checkEmptyFields()
         return true;
     }
 }
+function UpdateEmploymentInfo()
+{
+    if(checkEmptyFields())
+    {
+        let endpoint = apiAddressEmploymentService + 'employees/' + employeeSelected.id;
+        const newJobId = document.getElementById('selectJob');
+        let newOfficeId = document.getElementById('selectOffice');
+        let newEmergPhone = document.getElementById('emergPNumber').value;
+
+
+            //data to send to the api
+        let dataToUpdate = [
+            { op: 'replace', path: '/jobId', value: newJobId.options[newJobId.selectedIndex].value },
+            { op: 'replace', path: '/officeId', value: newOfficeId.options[newOfficeId.selectedIndex].value }
+        ];
+        patchEmploymentData(dataToUpdate, endpoint);
+    }
+}
+function patchEmploymentData(dataToUpdate, endpoint)
+{
+    console.log(dataToUpdate);
+    fetch(endpoint, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers if needed
+        },
+        body: JSON.stringify(dataToUpdate) // Convert your data to JSON
+    })
+    .then(response => {
+    // Handle the response from the server
+        if (response.ok) 
+        {
+            console.log('PATCH request successful');
+            alert('Data Successfully updated')
+            // Parse the response JSON
+            return response.json();
+
+        } else {
+            throw new Error('PATCH request failed');
+        }
+    })
+    .then(updatedData => {
+        // Log the updated data to the console
+        console.log('Updated Data:', updatedData);
+
+        // UPDATE UI
+
+
+        const departmentElement = document.getElementById('jobDepartment');
+        departmentElement.innerHTML = `<b>${updatedData.job.department} Department</b>`;
+
+        const jobRoleElement = document.getElementById('jobRole');
+        const jobRole2Element = document.getElementById('jobRole1');
+        jobRoleElement.innerHTML = `<b>${updatedData.job.jobTitle}</b>`;
+        jobRole2Element.innerHTML = `${updatedData.job.jobTitle}`;
+
+        const jobSalaryElement = document.getElementById('jobSalary');
+        jobSalaryElement.innerHTML = `<b>${updatedData.job.salary}</b>`;
+
+        const officeNumElement = document.getElementById('officeNum');
+        officeNumElement.innerHTML = `<b>${updatedData.office.buildingNum}</b>`;
+
+        const officeStreetElement = document.getElementById('officeStreet');
+        officeStreetElement.innerHTML = `<b>${updatedData.office.street}</b>`;
+
+        const officePostcodeElement = document.getElementById('officePostcode');
+        officePostcodeElement.innerHTML = `<b>${updatedData.office.postcode}</b>`;
+
+        const officeCityElement = document.getElementById('officeCity');
+        officeCityElement.innerHTML = `<b>${updatedData.office.city}</b>`;
+
+        const officeCountryElement = document.getElementById('officeCountry');
+        officeCountryElement.innerHTML = `<b>${updatedData.office.country}</b>`;
+
+    })
+    .catch(error => 
+    {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+}
 
 function UpdateEmergencyInfo()
 {
@@ -307,63 +389,60 @@ function patchEmergencyContact(dataToUpdate)
 
 }
 
-function enableEditing() {
-    // This function makes the <div> editable by adding the contenteditable attribute
-    const divToEdit = document.getElementById('editEmergencyContact');
+function fillSelectOptions(data, selectId, errorMessage, selectedEmployeeData) {
+    console.log(selectedEmployeeData);
+    const selectElement = document.getElementById(selectId);
 
-    const fNameEdit = document.getElementById('emergencyFName');
-    const lNameEdit = document.getElementById('emergencyLName');
-    const relationshipEdit = document.getElementById('emergRelationship');
-    const emergPhoneEdit = document.getElementById('EmergPhoneNum');
+    // Clear existing options
+    selectElement.innerHTML = '';
 
-    fNameEdit.contentEditable = true;
-    fNameEdit.classList.add('editable');
-    lNameEdit.contentEditable = true;
-    lNameEdit.classList.add('editable');
-    relationshipEdit.contentEditable = true;
-    relationshipEdit.classList.add('editable');
-    emergPhoneEdit.contentEditable = true;
-    emergPhoneEdit.classList.add('editable');
-    //divToEdit.contentEditable = true;
-    if(!isEmergButtonAdded)
-    {
-        const button = document.createElement('button');
-        button.textContent = 'Update';
-        button.classList.add('w3-button', 'w3-teal', 'w3-margin-top');
-        button.addEventListener('click', function() {
-        // Add your edit functionality here
-        let newFName = fNameEdit.textContent;
-        let newLName = lNameEdit.textContent;
-        let newRelationship = relationshipEdit.textContent;
-        let newEmergPhone = emergPhoneEdit.textContent;
-        console.log('Button clicked! newFName - ' + newFName);
+    // Check if data is available
+    if (data && Array.isArray(data) && data.length > 0) {
+        // IF its Job Data
+        if(selectId === 'selectJob')
+        {
+            // Create options based on data
+            data.forEach(item => {
+                const option = document.createElement('option');
+                option.value = item.id; 
+                option.textContent = item.jobTitle; // Assuming each item has a 'name' property
+                selectElement.appendChild(option);
+                if(item.jobTitle === selectedEmployeeData.job.jobTitle)
+                {
+                    // Set the users job as the selected item
+                    console.log('We Here');
+                    selectElement.value = item.id;
+                }
+            });
+        }
+        // IF its office data
+        if(selectId === 'selectOffice')
+        {
+            // Create options based on data
+            data.forEach(item => {
+                const option = document.createElement('option');
+                option.value = item.id; 
+                option.textContent = item.city + ', ' + item.country; // Assuming each item has a 'name' property
+                selectElement.appendChild(option);
+                if(item.id === selectedEmployeeData.office.id)
+                {
+                    // Set the first option as selected
+                    selectElement.value = item.id;
+                }
+            });
+        }
 
-        //data to send to the api
-        let dataToUpdate = [
-        { op: 'replace', path: '/fName', value: newFName },
-        { op: 'replace', path: '/lName', value: newLName },
-        { op: 'replace', path: '/Relationship', value: newRelationship },
-        { op: 'replace', path: '/pNumber', value: newEmergPhone }
-        ];
-            //fName: newFName, lName: newLName, Relationship: newRelationship, pNumber: newEmergPhone
-
-        
-        console.log(dataToUpdate);
-        patchEmergencyContact(dataToUpdate);
-    });
-    divToEdit.appendChild(button);
-    isEmergButtonAdded = true;
-
+    } else {
+        // If data is not available, show an error message
+        const errorOption = document.createElement('option');
+        errorOption.textContent = errorMessage;
+        selectElement.appendChild(errorOption);
     }
 
-
-}
-function editText(element) {
-    // This function makes the <span> inside the <div> editable
-    element.contentEditable = true;
-    element.classList.add('editableSpan');
 }
 
+function setNameText()
+{
     // handling name element
     const empNameElement = document.getElementById('empName');
     let employeeName = employeeSelected.fName.concat(" ", employeeSelected.lName);
@@ -374,6 +453,8 @@ function editText(element) {
         //Handling phone number element
     const phoneNumElement = document.getElementById('EmpPhoneNum');
     phoneNumElement.textContent = employeeSelected.pNumber;
+}
+
 
 
 // Performing all of the GET requests concurrently
@@ -382,12 +463,16 @@ function editText(element) {
 let apiAddressEndpoint = apiAddress + 'addresses/' + employeeSelected.id;
 let apiEmergencyEndpoint = apiAddress + 'emContacts/' + employeeSelected.id;
 let apiEmployeeJobEndpoint = apiAddressEmploymentService + 'employees/' + employeeSelected.id;
+let apiAllJobsEndpoint = apiAddressEmploymentService + 'jobs';
+let apiAllOfficesEndpoint = apiAddressEmploymentService + 'offices';
 
 // Create an array of fetch Promises
 let fetchPromises = [
     fetch(apiAddressEndpoint),
     fetch(apiEmergencyEndpoint),
-    fetch(apiEmployeeJobEndpoint)
+    fetch(apiEmployeeJobEndpoint),
+    fetch(apiAllJobsEndpoint),
+    fetch(apiAllOfficesEndpoint)
 ];
 
 // Execute all requests concurrently
@@ -402,7 +487,7 @@ Promise.all(fetchPromises)
         }));
     })
     .then(dataArray => {
-        const [addressData, emergencyData, employeeJobData] = dataArray;
+        const [addressData, emergencyData, employeeJobData, allJobsData, allOfficesData] = dataArray;
 
         // Work with address data
         const empCityCountryElement = document.getElementById('cityCountry');
@@ -448,11 +533,14 @@ Promise.all(fetchPromises)
         const relationshipFromDB = emergencyData.relationship;
         document.getElementById("relationship").value = relationshipMapping[relationshipFromDB];
 
+        // Usage example for jobs
+        fillSelectOptions(allJobsData, 'selectJob', 'Error - Data not loaded', employeeJobData);
 
-
-
+        // Usage example for offices
+        fillSelectOptions(allOfficesData, 'selectOffice', 'Error - Data not loaded', employeeJobData);
 
         // Work with employee job data
+        //selectedEmployeeData = employeeJobData;
         const departmentElement = document.getElementById('jobDepartment');
         departmentElement.innerHTML = `<b>${employeeJobData.job.department} Department</b>`;
 
